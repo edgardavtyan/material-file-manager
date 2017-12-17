4d551f3f3fa5f4dddc435d1e119e62a14305732b
Edgar Davtyan <edgar.davtyan@outlook.com>
2017-12-17 18:15:11 +0200

    Add 'empty directory' screen

--- app/src/main/java/com/davtyan/filemanager/components/entry/EntryActivity.java
+++ app/src/main/java/com/davtyan/filemanager/components/entry/EntryActivity.java
@@ -5,6 +5,8 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.davtyan.filemanager.R;
 import com.davtyan.filemanager.data.Storage;
@@ -14,6 +16,7 @@ import butterknife.ButterKnife;
 
 public class EntryActivity extends AppCompatActivity implements EntryMvp.View {
     @BindView(R.id.list) RecyclerView list;
+    @BindView(R.id.empty_directory_msg) LinearLayout emptyDirectoryView;
 
     private EntryMvp.Adapter adapter;
     private EntryPresenter presenter;
@@ -43,6 +46,14 @@ public class EntryActivity extends AppCompatActivity implements EntryMvp.View {
     @Override
     public void updateEntries(Storage[] entries) {
         adapter.updateEntries(entries);
+
+        if (entries.length == 0) {
+            list.setVisibility(View.GONE);
+            emptyDirectoryView.setVisibility(View.VISIBLE);
+        } else {
+            list.setVisibility(View.VISIBLE);
+            emptyDirectoryView.setVisibility(View.GONE);
+        }
     }
 
     @Override
