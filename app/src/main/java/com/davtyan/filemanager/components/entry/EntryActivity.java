b0b367f1130db814fd20341fea610ac34a87aac5
Edgar Davtyan <edgar.davtyan@outlook.com>
2017-12-19 02:29:16 +0200

    Display delete menu when items selected

--- app/src/main/java/com/davtyan/filemanager/components/entry/EntryActivity.java
+++ app/src/main/java/com/davtyan/filemanager/components/entry/EntryActivity.java
@@ -6,6 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SimpleItemAnimator;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -19,9 +22,12 @@ import butterknife.ButterKnife;
 public class EntryActivity extends AppCompatActivity {
     public static final String EXTRA_PATH = "extra_path";
 
+    private boolean deleteMenuEnabled;
+
     @BindView(R.id.list) RecyclerView list;
     @BindView(R.id.current_path) TextView currentPathView;
     @BindView(R.id.empty_directory_msg) LinearLayout emptyDirectoryView;
+    @BindView(R.id.toolbar) Toolbar toolbar;
 
     private EntryAdapter adapter;
     private EntryPresenter presenter;
@@ -31,6 +37,9 @@ public class EntryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry);
         ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+
+        deleteMenuEnabled = false;
 
         EntryModel model = new EntryModel();
         presenter = new EntryPresenter(this, model);
@@ -49,6 +58,17 @@ public class EntryActivity extends AppCompatActivity {
         presenter.onNavigateBack();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_entry, menu);
+
+        MenuItem deleteMenuItem = menu.findItem(R.id.menuitem_delete);
+        deleteMenuItem.setVisible(deleteMenuEnabled);
+        deleteMenuItem.setEnabled(deleteMenuEnabled);
+
+        return true;
+    }
+
     public void updateEntries(Storage[] entries) {
         adapter.updateEntries(entries);
 
@@ -63,10 +83,14 @@ public class EntryActivity extends AppCompatActivity {
 
     public void updateViewSelectionAt(int position) {
         adapter.notifyItemChanged(position);
+        deleteMenuEnabled = true;
+        invalidateOptionsMenu();
     }
 
     public void clearSelections() {
         adapter.notifyDataSetChanged();
+        deleteMenuEnabled = false;
+        invalidateOptionsMenu();
     }
 
     public void setCurrentPath(String path) {
