379ab7a70f2652b450737b18fd10981940db2229
Edgar Davtyan <edgar.davtyan@outlook.com>
2018-09-29 23:09:12 +0300

    Make SAF work with any sd card

--- app/src/main/java/com/davtyan/filemanager/lib/StorageAccessFramework.java
+++ app/src/main/java/com/davtyan/filemanager/lib/StorageAccessFramework.java
@@ -3,20 +3,28 @@ package com.davtyan.filemanager.lib;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.UriPermission;
 import android.net.Uri;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
 import android.support.v4.provider.DocumentFile;
 
 @TargetApi(21)
 public class StorageAccessFramework {
     private static final int REQUEST_MAIN = 1;
-    private static final String SDCARD_NAME = "7875-6541/";
-    private static final String URI_BASE_SDCARD = "content://com.android.externalstorage.documents/tree/7875-6541%3A/document/7875-6541%3A";
+    private static final String PREF_SAF_URI = "pref_saf_uri";
 
     private final Activity activity;
+    private final String sdCardName;
+    private final String sdCardEnding;
+    private final SharedPreferences prefs;
 
     public StorageAccessFramework(Activity activity) {
         this.activity = activity;
+        this.sdCardName = getSdCardName();
+        this.sdCardEnding = sdCardName + "%3A";
+        this.prefs = PreferenceManager.getDefaultSharedPreferences(activity);
     }
 
     public void makeAccessRequest() {
@@ -27,9 +35,10 @@ public class StorageAccessFramework {
     }
 
     public void persistPermissions(int requestCode, Intent intent) {
-        if (requestCode == REQUEST_MAIN) {
+        if (requestCode == REQUEST_MAIN && intent.getData().toString().endsWith(sdCardEnding)) {
             int flags = Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION;
             activity.getContentResolver().takePersistableUriPermission(intent.getData(), flags);
+            prefs.edit().putString(PREF_SAF_URI, intent.getData().toString()).apply();
         }
     }
 
@@ -41,9 +50,21 @@ public class StorageAccessFramework {
         getDocumentFile(filePath).renameTo(newName);
     }
 
+    @Nullable
+    public String getSdCardName() {
+        if (activity.getExternalCacheDirs().length < 2) {
+            return null;
+        }
+
+        String sdCardPath = activity.getExternalCacheDirs()[1].getPath();
+        int sdCardNameStart = sdCardPath.indexOf('/', 1) + 1;
+        int sdCardNameEnd = sdCardPath.indexOf('/', sdCardNameStart);
+        return sdCardPath.substring(sdCardNameStart, sdCardNameEnd);
+    }
+
     private boolean isPermissionGranted() {
         for (UriPermission uriPermission : activity.getContentResolver().getPersistedUriPermissions()) {
-            if (uriPermission.getUri().toString().endsWith("7875-6541%3A")) {
+            if (uriPermission.getUri().toString().endsWith(sdCardEnding)) {
                 return true;
             }
         }
@@ -52,13 +73,24 @@ public class StorageAccessFramework {
     }
 
     private DocumentFile getDocumentFile(String filePath) {
-        String[] fileParts = filePath.substring(filePath.indexOf(SDCARD_NAME) + SDCARD_NAME.length()).split("/");
-        DocumentFile resultDocument = DocumentFile.fromTreeUri(activity, Uri.parse(URI_BASE_SDCARD));
+        String[] fileParts = filePath
+                .substring(filePath.indexOf(sdCardName) + sdCardName.length() + 1)
+                .split("/");
+        DocumentFile resultDocument = getRootFile();
 
         for (String filePart : fileParts) {
             resultDocument = resultDocument.findFile(filePart);
         }
 
         return resultDocument;
+    }
+
+    private DocumentFile getRootFile() {
+        return DocumentFile.fromTreeUri(activity, Uri.parse(getSafUri()));
+    }
+
+    @Nullable
+    private String getSafUri() {
+        return prefs.getString(PREF_SAF_URI, null);
     }
 }
