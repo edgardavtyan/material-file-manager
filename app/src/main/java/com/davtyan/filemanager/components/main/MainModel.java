22979972b00c8aba4a014cb8c8bd39f88defc890
Edgar Davtyan <edgar.davtyan@outlook.com>
2018-11-04 15:50:51 +0200

    Remove unused getter

--- app/src/main/java/com/davtyan/filemanager/components/main/MainModel.java
+++ app/src/main/java/com/davtyan/filemanager/components/main/MainModel.java
@@ -26,8 +26,8 @@ public class MainModel {
     private @Getter Entry externalRoot;
     private @Getter Entry[] entries;
     private @Getter List<Entry> selectedEntries;
-    private @Getter List<Entry> copyEntries;
     private @Getter String currentDir;
+    private List<Entry> copyEntries;
     private boolean hasExternalStorage;
     private boolean isCutting;
 
