0cd257e1d784ab8f868233e0d62bfb39e21d708a
Edgar Davtyan <edgar.davtyan@outlook.com>
2018-11-04 16:27:10 +0200

    Delete unused methods

--- app/src/main/java/com/davtyan/filemanager/data/Entry.java
+++ app/src/main/java/com/davtyan/filemanager/data/Entry.java
@@ -2,11 +2,8 @@ package com.davtyan.filemanager.data;
 
 import java.io.File;
 
-import lombok.Getter;
-
 public class Entry {
     private File file;
-    private @Getter boolean isSelected;
 
     public Entry(File file) {
         this.file = file;
@@ -24,42 +21,12 @@ public class Entry {
         return file.getAbsolutePath();
     }
 
-    public long getFreeSpace() {
-        return file.getFreeSpace();
-    }
-
-    public long getTotalSpace() {
-        return file.getTotalSpace();
-    }
-
     public boolean isDirectory() {
         return file.isDirectory();
     }
 
     public boolean isFile() {
         return file.isFile();
-    }
-
-    public boolean delete() {
-        deleteRecursive(file);
-        return !file.exists();
-    }
-
-    public boolean renameTo(String newName) {
-        File directory = file.getParentFile();
-        return file.renameTo(new File(directory, newName));
-    }
-
-    public void setSelected(boolean isSelected) {
-        this.isSelected = isSelected;
-    }
-
-    public void clearSelection() {
-        isSelected = false;
-    }
-
-    public boolean exists() {
-        return file.exists();
     }
 
     private void deleteRecursive(File fileOrDirectory) {
